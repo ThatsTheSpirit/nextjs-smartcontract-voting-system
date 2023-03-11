b495ceda307f13dffd586c8a609a12fbabe38ecd
Atmos <1999bag@gmail.com>
2023-03-11 23:33:19 +0300

    First commit

---
+++ components/VotingEngineEntrance.jsx
@@ -0,0 +1,6 @@
+import { abi, contractAddresses } from "@/constants"
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import { useEffect, useState } from "react"
+import { useNotification } from "web3uikit"
+import { ethers } from "ethers"
+export default function VotingEngineEntrance() {}

1d02cac86588cbab1a123325e6580e7566d508c3
Atmos <1999bag@gmail.com>
2023-03-11 23:40:50 +0300

    Second commit

---
+++ components/ManualHeader.jsx
@@ -0,0 +1,7 @@
+// This file is to show what making a connect button looks like behind the scenes!
+
+import { useEffect } from "react"
+import { useMoralis } from "react-moralis"
+
+// Top navbar
+export default function ManualHeader() {}

ff3380180bfad08b9d4bfd80c8fe03d6d1315c8e
Atmos <1999bag@gmail.com>
2023-05-27 17:11:11 +0300

    small changes

--- pages/votings/[id]/results.js
+++ pages/votings/[id]/results.js
@@ -18,11 +18,32 @@ export default function Results() {
     const [candidates, setCandidates] = useState([])
     const [winner, setWinner] = useState("")
     const [candVotes, setCandVotes] = useState([])
+    const [question, setQuestion] = useState("")
 
     function getRandomInt(min, max) {
         min = Math.ceil(min)
         max = Math.floor(max)
         return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+
+    // function* zip(...iterables) {
+    //     let iterators = iterables.map((i) => i[Symbol.iterator]())
+    //     while (true) {
+    //         let results = iterators.map((iter) => iter.next())
+    //         if (results.some((res) => res.done)) return
+    //         else yield results.map((res) => res.value)
+    //     }
+    // }
+
+    function zip(arr1, arr2) {
+        let result = []
+        const longest = arr1.length > arr2.length ? arr1.length : arr2.length
+
+        for (let i = 0; i < longest; i++) {
+            result.push([arr1[i], arr2[i]])
+        }
+
+        return result
     }
 
     const { runContractFunction: getVoting } = useWeb3Contract({
@@ -43,7 +64,18 @@ export default function Results() {
         abi: votingAbi,
         contractAddress: votingAddress,
         functionName: "getWinner",
-        //params: { index: id },
+    })
+
+    const { runContractFunction: getQuestion } = useWeb3Contract({
+        abi: votingAbi,
+        contractAddress: votingAddress,
+        functionName: "getQuestion",
+    })
+
+    const { runContractFunction: getCandidatesVotes } = useWeb3Contract({
+        abi: votingAbi,
+        contractAddress: votingAddress,
+        functionName: "getCandidatesVotes",
     })
 
     async function updateVotingInfo() {
@@ -70,11 +102,17 @@ export default function Results() {
             setCandidates(candidatesFromCall)
             console.log(candidatesFromCall)
 
-            let candInfo = candidatesFromCall.map((candidate) => {
+            const allVotesFromCall = await getCandidatesVotes()
+            const allVotesNums = allVotesFromCall?.map((el) => Number(el))
+
+            let candZipped = zip(candidatesFromCall, allVotesNums)
+            console.log(candZipped)
+
+            const candInfo = candZipped.map(([cand, votes]) => {
                 return {
-                    id: candidate,
-                    label: candidate,
-                    value: 50, //get real data
+                    id: cand,
+                    label: cand,
+                    value: votes,
                     color: `hsl(${getRandomInt(0, 255)}, 70%, 50%)`,
                 }
             })
@@ -85,163 +123,147 @@ export default function Results() {
             const winnerFromCall = await getWinner()
             setWinner(winnerFromCall)
             console.log(winnerFromCall)
+
+            const qFromCall = await getQuestion()
+            setQuestion(qFromCall)
         }
         getInfo()
     }, [votingAddress])
 
-    const data = [
-        {
-            id: "make",
-            label: "make",
-            value: 479,
-            color: "hsl(8, 70%, 50%)",
-        },
-        {
-            id: "elixir",
-            label: "elixir",
-            value: 229,
-            color: "hsl(20, 70%, 50%)",
-        },
-        {
-            id: "erlang",
-            label: "erlang",
-            value: 205,
-            color: "hsl(211, 70%, 50%)",
-        },
-        {
-            id: "stylus",
-            label: "stylus",
-            value: 110,
-            color: "hsl(185, 70%, 50%)",
-        },
-        {
-            id: "java",
-            label: "java",
-            value: 577,
-            color: "hsl(103, 70%, 50%)",
-        },
-    ]
+    useEffect(() => {
+        console.log(candidates)
+        console.log(candVotes)
+        console.log(winner)
+        console.log(candVotes)
+    })
+
     return (
         <div style={{ height: 800 }}>
-            <ResponsivePie
-                //height={600}
-                //width={600}
-                data={candVotes}
-                margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-                innerRadius={0.5}
-                padAngle={0.7}
-                cornerRadius={3}
-                activeOuterRadiusOffset={8}
-                borderWidth={1}
-                borderColor={{
-                    from: "color",
-                    modifiers: [["darker", 0.2]],
-                }}
-                arcLinkLabelsSkipAngle={10}
-                arcLinkLabelsTextColor="#333333"
-                arcLinkLabelsThickness={2}
-                arcLinkLabelsColor={{ from: "color" }}
-                arcLabelsSkipAngle={10}
-                arcLabelsTextColor={{
-                    from: "color",
-                    modifiers: [["darker", 2]],
-                }}
-                defs={[
-                    {
-                        id: "dots",
-                        type: "patternDots",
-                        background: "inherit",
-                        color: "rgba(255, 255, 255, 0.3)",
-                        size: 4,
-                        padding: 1,
-                        stagger: true,
-                    },
-                    {
-                        id: "lines",
-                        type: "patternLines",
-                        background: "inherit",
-                        color: "rgba(255, 255, 255, 0.3)",
-                        rotation: -45,
-                        lineWidth: 6,
-                        spacing: 10,
-                    },
-                ]}
-                fill={[
-                    {
-                        match: {
-                            id: "ruby",
+            <div style={{ textAlign: "center", fontSize: "1.5em" }}>{question}</div>
+            {winner != "" ? (
+                <ResponsivePie
+                    //height={600}
+                    //width={600}
+
+                    data={candVotes}
+                    margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                    innerRadius={0.5}
+                    padAngle={0.7}
+                    cornerRadius={3}
+                    activeOuterRadiusOffset={8}
+                    borderWidth={1}
+                    borderColor={{
+                        from: "color",
+                        modifiers: [["darker", 0.2]],
+                    }}
+                    arcLinkLabelsSkipAngle={10}
+                    arcLinkLabelsTextColor="#333333"
+                    arcLinkLabelsThickness={2}
+                    arcLinkLabelsColor={{ from: "color" }}
+                    arcLabelsSkipAngle={10}
+                    arcLabelsTextColor={{
+                        from: "color",
+                        modifiers: [["darker", 2]],
+                    }}
+                    defs={[
+                        {
+                            id: "dots",
+                            type: "patternDots",
+                            background: "inherit",
+                            color: "rgba(255, 255, 255, 0.3)",
+                            size: 4,
+                            padding: 1,
+                            stagger: true,
                         },
-                        id: "dots",
-                    },
-                    {
-                        match: {
-                            id: "c",
+                        {
+                            id: "lines",
+                            type: "patternLines",
+                            background: "inherit",
+                            color: "rgba(255, 255, 255, 0.3)",
+                            rotation: -45,
+                            lineWidth: 6,
+                            spacing: 10,
                         },
-                        id: "dots",
-                    },
-                    {
-                        match: {
-                            id: "go",
-                        },
-                        id: "dots",
-                    },
-                    {
-                        match: {
-                            id: "python",
-                        },
-                        id: "dots",
-                    },
-                    {
-                        match: {
-                            id: "scala",
-                        },
-                        id: "lines",
-                    },
-                    {
-                        match: {
-                            id: "lisp",
-                        },
-                        id: "lines",
-                    },
-                    {
-                        match: {
-                            id: "elixir",
-                        },
-                        id: "lines",
-                    },
-                    {
-                        match: {
-                            id: "javascript",
-                        },
-                        id: "lines",
-                    },
-                ]}
-                legends={[
-                    {
-                        anchor: "bottom",
-                        direction: "row",
-                        justify: false,
-                        translateX: 0,
-                        translateY: 56,
-                        itemsSpacing: 0,
-                        itemWidth: 100,
-                        itemHeight: 18,
-                        itemTextColor: "#999",
-                        itemDirection: "left-to-right",
-                        itemOpacity: 1,
-                        symbolSize: 18,
-                        symbolShape: "circle",
-                        effects: [
-                            {
-                                on: "hover",
-                                style: {
-                                    itemTextColor: "#000",
-                                },
+                    ]}
+                    fill={[
+                        {
+                            match: {
+                                id: "ruby",
                             },
-                        ],
-                    },
-                ]}
-            />
+                            id: "dots",
+                        },
+                        {
+                            match: {
+                                id: "c",
+                            },
+                            id: "dots",
+                        },
+                        {
+                            match: {
+                                id: "go",
+                            },
+                            id: "dots",
+                        },
+                        {
+                            match: {
+                                id: "python",
+                            },
+                            id: "dots",
+                        },
+                        {
+                            match: {
+                                id: "scala",
+                            },
+                            id: "lines",
+                        },
+                        {
+                            match: {
+                                id: "lisp",
+                            },
+                            id: "lines",
+                        },
+                        {
+                            match: {
+                                id: "elixir",
+                            },
+                            id: "lines",
+                        },
+                        {
+                            match: {
+                                id: "javascript",
+                            },
+                            id: "lines",
+                        },
+                    ]}
+                    legends={[
+                        {
+                            anchor: "bottom",
+                            direction: "row",
+                            justify: false,
+                            translateX: 0,
+                            translateY: 56,
+                            itemsSpacing: 0,
+                            itemWidth: 100,
+                            itemHeight: 18,
+                            itemTextColor: "#999",
+                            itemDirection: "left-to-right",
+                            itemOpacity: 1,
+                            symbolSize: 18,
+                            symbolShape: "circle",
+                            effects: [
+                                {
+                                    on: "hover",
+                                    style: {
+                                        itemTextColor: "#000",
+                                    },
+                                },
+                            ],
+                        },
+                    ]}
+                />
+            ) : (
+                <h1>Winner did not define</h1>
+            )}
         </div>
     )
 }
